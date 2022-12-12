258d217a4887a5cfc6ce96e94a545959e1dfdfed
Elizabeth <elizabeth_c_wu@brown.edu>
2022-12-12 06:16:04 -0500

    fixed link

--- filter-tut/pages/components/arrows.js
+++ filter-tut/pages/components/arrows.js
@@ -5,11 +5,11 @@ export default function PageArrows(props) {
     console.log(props.prevPage);
     return (
         <div className={styles.container}>
-            <Link href={props.prevPage}>
+            <Link href={props.prevPage ?? ""}>
                 <span className={styles.arrow}>&larr;</span>
             </Link>
             <p className={styles.page}>(Page {props.pageNum})</p>
-            <Link href={props.nextPage}>
+            <Link href={props.nextPage ?? ""}>
                 <span className={styles.arrow}>&rarr;</span>
             </Link>
         </div>
